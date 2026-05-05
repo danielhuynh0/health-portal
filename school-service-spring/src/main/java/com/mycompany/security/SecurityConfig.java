3ff55369c110fafc3fc00f744a7b5a3942afaec7
Daniel Huynh <daniel.huynh@jhuapl.edu>
2026-05-05 13:02:10 -0400

    Fix cors again

--- school-service-spring/src/main/java/com/mycompany/security/SecurityConfig.java
+++ school-service-spring/src/main/java/com/mycompany/security/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(Customizer.withDefaults())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
